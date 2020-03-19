18be00cde1acccd275fd5de78794cda8697ea314
joaovictor <joaovictorengp@gmail.com>
2020-03-19 17:42:30 -0300

    API GoBarber completed

---
+++ modulo03/src/routes.js
@@ -0,0 +1,49 @@
+import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+// import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+// routes.get('/', async (req, res) => {
+//   const user = await User.create({
+//     name: 'João Mesquita',
+//     email: 'joao@mesquita93.com.br',
+//     password_hash: '1234',
+//   });
+//   return res.json(user);
+// });
+
+export default routes;
