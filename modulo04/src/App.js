63a03cbb8587c9ea7db9b5b81c2d3e10266f870c
joaovictor <joaovictorengp@gmail.com>
2020-03-22 21:29:38 -0300

    Introdução ao React

---
+++ modulo04/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import TechList from './components/TechList';
+
+// import profile from './assets/profile.jpeg';
+
+function App() {
+  return <TechList />;
+  // return <img width='400' src={profile} />;
+  // return <h1>Teste Corona Vairus !</h1>;
+  // return <h1>Welcome JV!!!</h1>;
+}
+
+export default App;
