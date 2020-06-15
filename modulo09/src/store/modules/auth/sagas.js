fac066f453f97ebedce308bd9bf997ef10b73a51
joaovictor <joaovictorengp@gmail.com>
2020-06-15 02:59:37 -0300

    GoBarber Web - User Registration and Authentication - OK

---
+++ modulo09/src/store/modules/auth/sagas.js
@@ -0,0 +1,68 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { token, user } = response.data;
+
+    if (!user.provider) {
+      toast.error(
+        'User is not a provider. Maybe you need to access mobile version !'
+      );
+      yield put(signFailure());
+      // console.tron.error('User is not a provider !');
+      return;
+    }
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    toast.error(
+      'Authentication Failure. Incorrect email or password. Please check !'
+    );
+    yield put(signFailure());
+  }
+}
+
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', { name, email, password, provider: true });
+    history.push('/');
+  } catch (err) {
+    toast.error('Registration Failure. Please check !');
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
--- modulo09/src/styles/global.js
+++ modulo09/src/styles/global.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import 'react-toastify/dist/ReactToastify.css';
+
 export default createGlobalStyle`
   /* @import url("https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap"); */
 
@@ -41,5 +43,38 @@ export default createGlobalStyle`
   button {
     cursor: pointer;
   }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active {
+    box-shadow: 0 0 0px 1000px #232129 inset;
+    transition: 'color 9999s ease-out, background-color 9999s ease-out';
+    transition-delay: 9999s;
+  }
+
+  .Toastify__toast--error {
+  border: 1px solid #fff;
+  border-radius: 4px;
+  color: #fff;
+  /* font-weight: bold; */
+  background-color: #ff3434;
+}
+
+.Toastify__toast--success {
+  border: 2px solid #fff;
+  border-radius: 4px;
+  color: #fff;
+  font-weight: bold;
+}
+
+.Toastify__close-button {
+  color: #fff;
+  opacity:1
+}
+.Toastify__progress-bar {
+  height: 4px;
+  /* background-color: #000; */
+}
 
 `;
